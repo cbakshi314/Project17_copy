340affa9b54a5c9722b8df5fdb0b5d70966330de
Brian Farias <Brian03032003@gmail.com>
2023-11-16 09:40:50 -0500

    banner for share lesson button

--- client/src/views/Mentor/Dashboard/Dashboard.jsx
+++ client/src/views/Mentor/Dashboard/Dashboard.jsx
@@ -12,8 +12,11 @@ export default function Dashboard() {
   const [classrooms, setClassrooms] = useState([]);
   const [value] = useGlobalState('currUser');
   const[userId, setUserId] = useState([]);
-  const[inboxSize, setInboxSize] = useState(0);
+  const[inboxSize, setInboxSize] = useState(null);
   const navigate = useNavigate();
+
+  const banner = document.getElementsByClassName('banner');
+
 
   useEffect(() => {
     let classroomIds = [];
@@ -21,6 +24,9 @@ export default function Dashboard() {
       if (res.data) {
         setUserId(res.data.id);
         setInboxSize(res.data.inbox.length)
+        if(res.data.inbox.length == 0){
+          banner.display = none;
+        }
         res.data.classrooms.forEach((classroom) => {
           classroomIds.push(classroom.id);
         });
@@ -46,7 +52,7 @@ export default function Dashboard() {
   return (
     <div className='container nav-padding'>
       <NavBar />
-      <div id='main-header'>Welcome {value.name} <button id='share' onClick={() => viewInbox(userId)}> Shared Lessons <strong>({inboxSize})</strong> </button> </div>
+      <div id='main-header'>Welcome {value.name} <button id='share' onClick={() => viewInbox(userId)}> Shared Lessons <div className='banner'><p>{inboxSize}</p></div> </button> </div>
       <MentorSubHeader title={'Your Classrooms'}></MentorSubHeader>
       <div id='classrooms-container'>
         <div id='dashboard-card-container'>

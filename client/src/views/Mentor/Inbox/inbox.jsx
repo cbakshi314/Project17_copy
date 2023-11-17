97b6fba593b97a81d96369cd7bbac5886cf653da
Brian Farias <Brian03032003@gmail.com>
2023-11-17 16:16:08 -0500

    can view inbox

--- client/src/views/Mentor/Inbox/inbox.jsx
+++ client/src/views/Mentor/Inbox/inbox.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import { getMentor, getClassrooms } from '../../../Utils/requests';
+import { getMentor, getLessonModuleActivities } from '../../../Utils/requests';
 import { Tabs } from 'antd';
 import MentorSubHeader from '../../../components/MentorSubHeader/MentorSubHeader';
 import NavBar from '../../../components/NavBar/NavBar';
@@ -10,11 +10,45 @@ import './Inbox.less';
 const { TabPane } = Tabs;
 
 export default function Inbox() {
+    const [printed, setprinted] = useState(false);
     const navigate = useNavigate();
-
     const handleBack = () => {
         navigate('/dashboard');
       };
+
+      useEffect(() => {
+        const dataBox = document.getElementById('inboxData');
+        getMentor().then((res) => {
+          if (res.data && !printed) {
+            res.data.inbox.forEach((lesson) => {
+                console.log(lesson);
+                const box = document.createElement('div');
+                box.className = 'item';
+                box.onclick = () => expand(lesson);
+                const text1 = document.createElement('h2');
+                text1.innerHTML = `${lesson.name}`;
+                const text2 = document.createElement('p');
+                text2.innerHTML = `Expectations: ${lesson.expectations}`;
+                const text3 = document.createElement('p');
+                if(less)
+                text2.innerHTML = `Expectations: ${lesson.expectations}`;
+                box.append(text1);
+                box.append(text2);
+                box.append(text3);
+                dataBox.append(box);
+            });
+            setprinted(true);
+          } 
+          else if(!printed){
+            message.error(res.err);
+            navigate('/teacherlogin');
+          }
+        });
+      }, []);
+
+    const expand = (lesson) =>{
+        console.log(lesson)
+    }
 
   return (
     <div className='container nav-padding'>
@@ -24,7 +58,8 @@ export default function Inbox() {
             <button id='home-back-btn1' onClick={handleBack}>
                 <i className='fa fa-arrow-left' aria-hidden='true' />
             </button>
-            <div className='inboxData'>
+            <h1 className='title'>Recently Shared with You</h1>
+            <div id='inboxData'>
 
             </div>
         </div>

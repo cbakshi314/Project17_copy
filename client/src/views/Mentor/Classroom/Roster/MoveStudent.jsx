8b39116bcb71b3c204a300e29dc5e3a2bf10a414
cbakshi314 <cbakshi@ufl.edu>
2023-11-26 10:36:05 -0500

    Working on moving students

--- client/src/views/Mentor/Classroom/Roster/MoveStudent.jsx
+++ client/src/views/Mentor/Classroom/Roster/MoveStudent.jsx
@@ -1,8 +1,13 @@
 import { Modal, Button } from 'antd';
 import React, { useState } from 'react';
+import {
+    getAllClassrooms
+} from '../../../../Utils/requests';
 
 export default function MoveStudent({ linkBtn, student, getFormattedDate }) {
   const [visible, setVisible] = useState(false);
+  const [classList, setClassList] = useState([]);
+    let allClassrooms = getAllClassrooms();
 
   const showModal = () => {
     setVisible(true);
@@ -36,19 +41,31 @@ export default function MoveStudent({ linkBtn, student, getFormattedDate }) {
           <h1 id='student-card-title'>{"Move " + student.name + " to another class"}</h1>
         </div>
         <div id='description-container'>
-              <p id='label'>Current class:</p>
+              <p id='label'>Current class:&nbsp;</p>
               <p id='label-info'> {"NAME OF CLASSROOM"}</p>
               <br></br>
         </div>
         <div id='description-container'>
-          <p id='label'>Status:</p>
+          <p id='label'>Status:&nbsp;</p>
           <p id='label-info'>
             {student.enrolled.enrolled ? 'Enrolled' : 'Unenrolled'}
           </p>
         </div>
         <div>
           <div id='modal-card-content-container'>
-            <p id='other-class-name'>{"NAME OF OTHER CLASSROOM"}</p>
+              <p id='label'>Choose destination classroom:</p>
+              <select
+                  id="classroom-dropdown"
+                  name="classroom"
+                  onChange={e => setClassList(e.target.value)} /////here, how do I save the selected classroom?
+                  required
+              >
+                  {allClassrooms.map(class_ => (
+                      <option key={class_.id} value={class_.id}>
+                          {class_.name}
+                      </option>
+                  ))}
+              </select>
           </div>
           <div id='modal-card-content-container'>
             
